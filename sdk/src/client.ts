ce4b74d2d916a2f4d1a87d78b123229e86174ccf
abassade <abs4real16.ma@gmail.com>
2026-06-24 21:38:20 +0100

    updated base url

--- sdk/src/client.ts
+++ sdk/src/client.ts
@@ -1,12 +1,12 @@
-import { HttpClient } from './http.js';
-import { DomainsResource } from './resources/domains.js';
-import { EmailsResource } from './resources/emails.js';
-import { SendersResource } from './resources/senders.js';
-import { TemplatesResource } from './resources/templates.js';
-import { WebhooksResource } from './resources/webhooks.js';
-import type { PostaSendOptions } from './types.js';
+import { HttpClient } from "./http.js";
+import { DomainsResource } from "./resources/domains.js";
+import { EmailsResource } from "./resources/emails.js";
+import { SendersResource } from "./resources/senders.js";
+import { TemplatesResource } from "./resources/templates.js";
+import { WebhooksResource } from "./resources/webhooks.js";
+import type { PostaSendOptions } from "./types.js";
 
-const DEFAULT_BASE_URL = 'https://api.postasend.com/v1';
+const DEFAULT_BASE_URL = "https://api.postasend.com/api/v1";
 
 export class PostaSend {
   readonly emails: EmailsResource;
@@ -16,7 +16,7 @@ export class PostaSend {
   readonly webhooks: WebhooksResource;
 
   constructor(options: PostaSendOptions) {
-    if (!options.apiKey) throw new Error('PostaSend: apiKey is required');
+    if (!options.apiKey) throw new Error("PostaSend: apiKey is required");
 
     const http = new HttpClient(
       options.baseUrl ?? DEFAULT_BASE_URL,
